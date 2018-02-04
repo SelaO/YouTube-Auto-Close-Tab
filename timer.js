46a56212519cd36b272461196a8dd9380c305b8e
SelaO <SelaO@users.noreply.github.com>
2018-02-04 14:38:20 +0200

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,12 +1,24 @@
+var timeoutvar;
 var video = document.getElementsByTagName("video")[0];
 
 if(video) {
   video.addEventListener("ended", function() {
-    setTimeout(msg, 60000);
+    console.log("Video ended");
+    
+    timeoutvar = setTimeout(msg, 90000);
+    
+    video.onmouseover = function() {mouseOver();};
+    
   });
 } 
 else {
   console.error("Video element not found");
+}
+
+//reset timeout if detected movement on the video
+function mouseOver() {
+     console.log("mouse over");
+     clearTimeout(timeoutvar);
 }
 
 function msg(){
